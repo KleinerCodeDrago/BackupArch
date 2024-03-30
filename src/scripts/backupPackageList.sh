23c26712d19adac6fa6db65735a6ff9f83a83c3f
AugenAugen <allthestuffilove@protonmail.com>
2024-03-30 14:55:37 +0100

    update script to backup snap-package-list

--- src/scripts/backupPackageList.sh
+++ src/scripts/backupPackageList.sh
@@ -1,2 +1,3 @@
 pacman -Qqen > archConfigs/packages/pkglist-repo.txt
 pacman -Qqem > archConfigs/packages/pkglist-aur.txt
+snap list --all > archConfigs/packages/snap-package-list.txt
--- src/scripts/restorePackages.sh
+++ src/scripts/restorePackages.sh
@@ -1,2 +1,8 @@
 sudo pacman -S --needed - < archConfigs/packages/pkglist-repo.txt
 for x in $(< archConfigs/packages/pkglist-aur.txt); do paru -S $x; done
+sudo pacman -S snapd
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+while read snap; do
+    sudo snap install "$snap"
+done < archConfigs/packages/snap-package-list.txt
